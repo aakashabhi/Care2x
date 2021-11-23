c9b5e6d1cc66e3a5826df9c13cfc19cd7747bcaf
deeptadevkota <deeptadevkota21@gmail.com>
2021-11-23 19:24:28 +0545

    commit

---
+++ care2x/lib/Order_history/order_history.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  final db = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Collection Data'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: db.collection('orders').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return new ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    var temp = snapshot.data!.docs[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: Column(
+                          children: <Widget>[
+                            Text(temp['email']),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            } else {
+              return Text('Loading');
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ care2x/lib/Order_history/subCategory.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class SubCategoery extends StatefulWidget {
+  final doc;
+  SubCategoery(this.doc);
+
+  @override
+  _SubCategoryClassState createState() => _SubCategoryClassState();
+}
+
+class _SubCategoryClassState extends State<SubCategoery> {
+  final db = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SubCategories"),
+        centerTitle: true,
+      ),
+      body: new StreamBuilder<QuerySnapshot>(
+        stream: db
+            .collection('orders')
+            .doc(widget.doc)
+            .collection('TestSubCollection')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return new ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var temp = snapshot.data!.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Column(
+                        children: <Widget>[
+                          Text(temp['quantity']),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          } else {
+            return LinearProgressIndicator();
+          }
+        },
+      ),
+    );
+  }
+}
